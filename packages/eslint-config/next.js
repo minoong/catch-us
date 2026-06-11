318cb0488280f82e329b285789b0dcb32d8d71b0
이민우 <alsdndjdrktl@gmail.com>
2026-06-11 22:20:04 +0900

    fix: complete shared React lint rules

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,13 +1,34 @@
 import nextPlugin from "@next/eslint-plugin-next";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
 import { baseConfig } from "./base.js";
 
 export const nextJsConfig = [
   ...baseConfig,
+  reactPlugin.configs.flat.recommended,
+  {
+    languageOptions: {
+      ...reactPlugin.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.browser,
+        ...globals.serviceworker,
+      },
+    },
+  },
   {
     plugins: { "@next/next": nextPlugin },
     rules: {
       ...nextPlugin.configs.recommended.rules,
       ...nextPlugin.configs["core-web-vitals"].rules,
+    },
+  },
+  {
+    plugins: { "react-hooks": reactHooks },
+    settings: { react: { version: "detect" } },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
     },
   },
 ];
--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -1,10 +1,26 @@
+import reactPlugin from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
 import { baseConfig } from "./base.js";
 
 export const reactInternalConfig = [
   ...baseConfig,
+  reactPlugin.configs.flat.recommended,
+  {
+    languageOptions: {
+      ...reactPlugin.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.browser,
+        ...globals.serviceworker,
+      },
+    },
+  },
   {
     plugins: { "react-hooks": reactHooks },
-    rules: reactHooks.configs.recommended.rules,
+    settings: { react: { version: "detect" } },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+    },
   },
 ];
